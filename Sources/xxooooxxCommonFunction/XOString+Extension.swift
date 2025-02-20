8f41037ff5f43b9a366e292cb17d3ad218913849
chris901003 <a0987999103@gmail.com>
2025-02-20 22:30:55 +0800

    <xxooooxxiOSLibrary> #10 feat: add email valid string extension

---
+++ Sources/xxooooxxCommonFunction/XOString+Extension.swift
@@ -0,0 +1,17 @@
+// Created for  in 2025
+// Using Swift 5.0
+//
+//
+// Created by HongYan on 2025/2/8.
+// Copyright © 2025 HongYan. All rights reserved.
+
+
+import Foundation
+
+public extension String {
+    func isValidEmail() -> Bool {
+        let emailRegex = "^[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: self)
+    }
+}
